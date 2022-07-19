79473fd8c563a9354e7aeb7a417cb9aff735403f
Nikhil Deo <89142086+Nikhildevnikhil@users.noreply.github.com>
2022-07-19 22:11:34 +0530

    https://leetcode.com/problems/flipping-an-image/submissions/

--- 832. Flipping an Image/main.cpp
+++ 832. Flipping an Image/main.cpp
@@ -11,8 +11,7 @@ public:
          
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(image[i][j] == 0) image[i][j] = 1;
-                else if (image[i][j] == 1) image[i][j] = 0;
+                image[i][j] = (image[i][j] == 0) ? 1 : 0;
             }  
         }
         return image;
